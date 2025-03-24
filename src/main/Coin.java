1afed34471839e2489cd82fbf6057f701c37a016
bmchaffin <chaffinbm@appstate.edu>
2025-03-24 12:13:40 -0400

    added fields and method headers

--- src/main/Coin.java
+++ src/main/Coin.java
@@ -1,3 +1,26 @@
 public class Coin {
-    
+    private double value;
+    private String commonName;
+    private String frontMotto;
+    private int manufactureYear;
+    private String frontImage;
+    private String backimage;
+    private String backMotto;
+    private String frontLabel;
+    private String backLabel;
+    private String valueDescription;
+    private boolean ridgedEdge;
+    private String metallurgy;
+    public Coin(){
+
+    }
+    public Coin(double value){
+
+    }
+    public Coin(double value, int year){
+
+    }
+    public String toString(){
+        
+    }
 }
