663fcf9591618abb8a27ef1cb8d92acb39d0d444
baldasarod <baldasarod@appstate.edu>
2025-04-02 12:40:05 -0400

    wrong branch

--- src/main/Copper.java
+++
@@ -1,9 +0,0 @@
-public class Copper implements Metallurgy{
-
-    @Override
-    public void Smelt() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Smelt'");
-    }
-    
-}
--- src/main/CuproNickel.java
+++
@@ -1,9 +0,0 @@
-public class CuproNickel implements Metallurgy {
-
-    @Override
-    public void Smelt() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Smelt'");
-    }
-    
-}
