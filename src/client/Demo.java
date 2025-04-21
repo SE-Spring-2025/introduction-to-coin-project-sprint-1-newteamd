a816437bcccfd8b3751d6cb4f474422148b44895
bmchaffin <chaffinbm@appstate.edu>
2025-04-21 12:41:09 -0400

    Modified demo to be interactive

--- src/client/Demo.java
+++ src/client/Demo.java
@@ -17,107 +17,86 @@ public class Demo {
 		System.out.println("Creating a penny to see total count go up but not quarter (with more delay)");
 		Thread.sleep(2000);
 		Coin penny = new Penny();
+		System.out.println("Demo complete (There should be 2 quarters and 3 total coins)");
+		interactiveDemo();
+
+		/*System.out.println();
+		System.out.println();
+		System.out.println("Welcome to the CoinFlips Demo!");
+
+		fixedDemo();
+		interactiveDemo();
 		
-
-
-
-
-
-	/*System.out.println();
-	System.out.println();
-	System.out.println("Welcome to the CoinFlips Demo!");
-
-	fixedDemo();
-	interactiveDemo();
-	
-	System.out.println();
-	System.out.println();
-	System.out.println("Thanks for watching the CoinFlips Demo.");
-	System.out.println("Have a wonderful day!");
-	System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("Thanks for watching the CoinFlips Demo.");
+		System.out.println("Have a wonderful day!");
+		System.out.println();*/
     }
     private static void fixedDemo() {
-	System.out.println();
-	System.out.println();
-	System.out.println("Making a Penny");
-	System.out.println("Result:" + new Penny());
-	System.out.println("Making a Dime");
-	System.out.println("Result:" + new Dime());
-	System.out.println("Making a Nickel");
-	System.out.println("Result:" + new Nickel());
-	System.out.println("Making a Quarter");
-	System.out.println("Result:" + new Quarter());
-	System.out.println("Making a HalfDollar");
-	System.out.println("Result:" + new HalfDollar());
-	System.out.println("Making a Dollar");
-	System.out.println("Result:" + new Dollar());
+		System.out.println();
+		System.out.println();
+		System.out.println("Making a Penny");
+		System.out.println("Result:" + new Penny());
+		System.out.println("Making a Dime");
+		System.out.println("Result:" + new Dime());
+		System.out.println("Making a Nickel");
+		System.out.println("Result:" + new Nickel());
+		System.out.println("Making a Quarter");
+		System.out.println("Result:" + new Quarter());
+		System.out.println("Making a HalfDollar");
+		System.out.println("Result:" + new HalfDollar());
+		System.out.println("Making a Dollar");
+		System.out.println("Result:" + new Dollar());
     }
     private static void printMenu() {
-	System.out.println();
-	System.out.println();
-	System.out.println("I'm ready to make you a coin!");
-	System.out.println("Enter G for a dollar coin.");
-	System.out.println("Enter H for a half dollar coin.");
-	System.out.println("Enter Q for a quarter  coin.");
-	System.out.println("Enter D for a dime  coin.");
-	System.out.println("Enter N for a nickel coin.");
-	System.out.println("Enter P for a penny coin.");
-	System.out.println();
-	System.out.println("Enter X to exit the demo.");
-	System.out.println();
+		System.out.println("I'm ready to make you a coin!");
+		System.out.println("Enter G for a dollar coin.");
+		System.out.println("Enter H for a half dollar coin.");
+		System.out.println("Enter Q for a quarter  coin.");
+		System.out.println("Enter D for a dime  coin.");
+		System.out.println("Enter N for a nickel coin.");
+		System.out.println("Enter P for a penny coin.");
+		System.out.println();
+		System.out.println("Close the dashboard window to exit the demo.");
+		System.out.println();
     }
     private static void interactiveDemo() {
-	System.out.println();
-	System.out.println();
-	System.out.println("Would you like to make more coins yourself? (Y/n) ");
-	String response = keyboard.nextLine();
-	response = response.trim();
-	if (response.charAt(0) == 'Y' || response.charAt(0) == 'y')
-	    runDemo();
-    }
-    private static void runDemo() {
-	Coin c;
-	
-
-	while (true) {
-	    System.out.println();
-	    System.out.println();
-
-	    printMenu();
-	    System.out.print("What coin to make? ");
-	    String response = keyboard.nextLine();
-	    response = response.trim();
-	    switch (response.charAt(0)) {
-	        case 'G':
-		    System.out.println("Gonna make a dollar coin...");
-		    c = new Dollar(1);
-		    break;
-	        case 'H':
-		    System.out.println("Gonna make a half dollar coin...");
-		    c = new HalfDollar(1);
-		    break;
-	        case 'Q':
-		    System.out.println("Gonna make a quarter coin...");
-		    c = new Quarter(1);
-		    break;
-	        case 'D':
-		    System.out.println("Gonna make a dime coin...");
-		    c = new Dime(1);
-		    break;
-	        case 'N':
-		    System.out.println("Gonna make a nickel coin...");
-			c = new Nickel(1);
-		    break;
-	        case 'P':
-		    System.out.println("Gonna make a penny coin...");
-		    c = new Penny(1);
-		    break;
-	        case 'X':
-		    return;
-	        default:
-		    System.out.println("Invalid entry, try again.");
-		    break;
-	    }
-		}*/
+		while (true) {
+			Coin c;
+			printMenu();
+			System.out.print("What coin to make? ");
+			String response = keyboard.nextLine();
+			response = response.trim().toUpperCase();
+			switch (response.charAt(0)) {
+				case 'G':
+				System.out.println("Gonna make a dollar coin...");
+				c = new Dollar(1);
+				break;
+				case 'H':
+				System.out.println("Gonna make a half dollar coin...");
+				c = new HalfDollar(1);
+				break;
+				case 'Q':
+				System.out.println("Gonna make a quarter coin...");
+				c = new Quarter(1);
+				break;
+				case 'D':
+				System.out.println("Gonna make a dime coin...");
+				c = new Dime(1);
+				break;
+				case 'N':
+				System.out.println("Gonna make a nickel coin...");
+				c = new Nickel(1);
+				break;
+				case 'P':
+				System.out.println("Gonna make a penny coin...");
+				c = new Penny(1);
+				break;
+				default:
+				System.out.println("Invalid entry, try again.");
+				break;
+			}
+			}
 	}
 }
